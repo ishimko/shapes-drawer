ccdfe3cc4480f2b42f2b077dd9dbd98e58e2f570
Ivan <vana.shimko@gmail.com>
2016-02-23 10:32:45 +0300

    modern loop

--- ShapesDrawer.java
+++ ShapesDrawer.java
@@ -7,22 +7,14 @@ import java.util.ArrayList;
 public class ShapesDrawer extends JPanel {
     private ArrayList<Shape> shapesList =  new ArrayList<>();
 
-    /*public ShapesDrawer(ArrayList<Shape> shapesList){
-        this.shapesList = (ArrayList<Shape>)shapesList.clone();
-    }*/
-
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        for (int i = 0; i < shapesList.size(); i++) {
-            shapesList.get(i).draw(g);
+        for (Shape shape : shapesList){
+            shape.draw(g);
         }
     }
 
     public void addShape(Shape f) {
         shapesList.add(f);
-    }
-
-    public void clearList() {
-        shapesList.clear();
     }
 }
