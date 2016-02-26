41d41302068e9eddadb0acf4e52c7d467b4d746a
Ivan <vana.shimko@gmail.com>
2016-02-26 18:38:17 +0300

    circle drawing

--- Circle.java
+++ Circle.java
@@ -5,14 +5,30 @@ public class Circle extends Ellipse {
         super(new Point(center.x - radius, center.y - radius), radius*2, radius*2);
     }
 
+//    @Override
+//    public void refreshShape(Point point){
+//        int newWidth = Math.abs(getFixedPoint().x - point.x);
+//        int newHeight = Math.abs(getFixedPoint().y - point.y);
+//
+//        if (newHeight < newWidth){
+//            super.refreshShape(new Point(getFixedPoint().x, point.y));
+//        } else {
+//            super.refreshShape(new Point(point.x, getFixedPoint().y));
+//        }
+//    }
+
     @Override
     public void draw(Graphics g){
         g.setColor(getBorderColor());
 
-        Point topLeft = getTopLeft();
-        Point bottomRight = getBottomRight();
-        int width = bottomRight.x - topLeft.x;
+        int width = getBottomRight().x - getTopLeft().x;
+        int height = getBottomRight().y - getTopLeft().y;
+        int d = Math.min(width, height);
 
-        g.drawOval(topLeft.x, topLeft.y, width, width);
+        Point fixedPoint = getFixedPoint();
+        int topX = fixedPoint.x + d * (int)Math.signum(getTopLeft().x - fixedPoint.x);
+        int topY = fixedPoint.y + d * (int)Math.signum(getTopLeft().y - fixedPoint.y);
+
+        g.drawOval(topX, topY, d, d);
     }
 }
--- Ellipse.java
+++ Ellipse.java
@@ -1,26 +1,30 @@
 import java.awt.*;
 
 public class Ellipse extends Shape{
-    protected Point a, b;
+    private Point fixedPoint, movingPoint;
     public Ellipse(Point topLeft, int width, int height) {
-        a = topLeft;
-        b = new Point(topLeft.x + width, topLeft.y + height);
+        fixedPoint = topLeft;
+        movingPoint = new Point(topLeft.x + width, topLeft.y + height);
+    }
+
+    protected Point getFixedPoint(){
+        return this.fixedPoint;
     }
 
     protected Point getTopLeft(){
-        int xMin = Math.min(a.x, b.x);
-        int yMin = Math.min(a.y, b.y);
+        int xMin = Math.min(fixedPoint.x, movingPoint.x);
+        int yMin = Math.min(fixedPoint.y, movingPoint.y);
 
         return new Point(xMin, yMin);
     }
 
     public void refreshShape(Point p){
-        b = new Point(p);
+        movingPoint = new Point(p);
     }
 
     protected Point getBottomRight(){
-        int xMax = Math.max(a.x, b.x);
-        int yMax = Math.max(a.y, b.y);
+        int xMax = Math.max(fixedPoint.x, movingPoint.x);
+        int yMax = Math.max(fixedPoint.y, movingPoint.y);
 
         return new Point(xMax, yMax);
     }
@@ -31,6 +35,6 @@ public class Ellipse extends Shape{
         Point topLeft = getTopLeft();
         Point bottomRight = getBottomRight();
 
-        g.drawOval(topLeft.x, topLeft.y, bottomRight.x - topLeft.x, bottomRight.y -topLeft.y);
+        g.drawOval(topLeft.x, topLeft.y, bottomRight.x - topLeft.x, bottomRight.y - topLeft.y);
     }
 }
